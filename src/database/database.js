2da4edd5db674b64dbbaddcbda6c41a9035e57ed
ted2901 <teddy.machicado.tito@gmail.com>
2026-03-08 00:05:06 -0400

    agregar ssl

--- src/database/database.js
+++ src/database/database.js
@@ -9,8 +9,11 @@ export const sequelize = new Sequelize(
         host: env.db_host,
         dialect: env.db_dialect,
         logging: console.log,
-        dialectOptions: env.db_use_ssl ? {
-
+        dialectOptions: env.db_use_ssl === 'true' ? {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         } : {}
     }
 );
